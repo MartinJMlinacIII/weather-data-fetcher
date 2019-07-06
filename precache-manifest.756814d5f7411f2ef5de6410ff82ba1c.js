689e29b4462fa19f5aac1b3a537f1802ed7b7ed6
MartinJMlinacIII <mlinacm@mail.gvsu.edu>
2019-07-05 20:14:49 -0700

    Updates

---
+++ precache-manifest.756814d5f7411f2ef5de6410ff82ba1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a426cef7f8101662aecf371da00b94",
+    "url": "/weather-data-fetcher/index.html"
+  },
+  {
+    "revision": "3edbab8c435d6dcc9cf1",
+    "url": "/weather-data-fetcher/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d9588abf7d5134da7e39",
+    "url": "/weather-data-fetcher/static/js/2.6335d946.chunk.js"
+  },
+  {
+    "revision": "3edbab8c435d6dcc9cf1",
+    "url": "/weather-data-fetcher/static/js/main.a3074fc7.chunk.js"
+  },
+  {
+    "revision": "6e3c1bf1778cb20c49c1",
+    "url": "/weather-data-fetcher/static/js/runtime~main.024eb39b.js"
+  }
+]);
